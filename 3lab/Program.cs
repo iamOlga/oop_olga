88eda030adf44e651c4f6d633ea501a69abb383f
Olga Babich <olgab2806@gmail.com>
2020-09-30 11:06:31 +0300

    modified 3 lab

--- 3lab/Program.cs
+++ 3lab/Program.cs
@@ -45,12 +45,6 @@ namespace _3lab
             }
 
         }
-        
-        ////статический конструктор
-        //static Vector()
-        //{
-        //    count = 0;
-        //}
 
         //индексатор
         public int this[int i]
@@ -66,10 +60,10 @@ namespace _3lab
         }
         public void Random()//заполнение массива
         {
-            Random random = new Random();
+            Random random = new Random(DateTime.Now.Millisecond);
             for (int i = 0; i < count; i++)
             { 
-                items[i] = random.Next(100);
+                items[i] = random.Next(10);
             }
         }
         public int[] Addition(int x) //сложение  эт-ов с числом
@@ -90,85 +84,123 @@ namespace _3lab
         }
         public void OutputArr()//вывод массива
         {
-            Console.WriteLine("\nВывод массива: ");
+            
             for (int i = 0; i < count; i++)
             {
                 Console.Write(items[i]+"\t");
             }
+            Console.Write("\n");
         }
-        public void OutputStatus()//вывод состояния статуса
+        public bool hasNull()
         {
-            Console.WriteLine("\nСостояние статуса: " + status);   
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
-
+        public double Module()
+        {
+            double module = 0;
+            for (int i = 0; i < count; i++)
+            {
+                module += Math.Abs(items[i]);
+            }
+            return module;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("1.Создание рандомного массива\n2.Вывод массива\n3.Сложение эл-ов с числом\n4.Умножение эл-ов на число");
+            Console.Write("1.Создание массива векторов\n2.Сложение эл-ов с числом\n3.Умножение эл-ов на число\n4.Векторов с 0\n5.Вектора с наим. модулем");
+            int status;
+            //Vector items1 = new Vector();//создание с помощью конструкторов
+            //Vector items2 = new Vector(20);
+            //Vector items3 = new Vector(5, 1);
 
-            Vector items1 = new Vector();
-            Vector items2 = new Vector(20);//создание с помощью других конструкторов
-            Vector items3 = new Vector(5, 1);
+            Vector[] items = new Vector[5];
             for (; ; )
             {
                 Console.Write("\nСделайте выбор: ");
-
                 int choice = int.Parse(Console.ReadLine());
                 if (choice <= 5)
                 {
                     switch (choice)
                     {
                         case 1:
-                            items1.Random();
-                            items1.status = 1;
-                            Console.WriteLine("Был создан массив");
-                            Console.WriteLine("Статус операции: " + items1.status);
+                            Console.WriteLine("\nВывод массива: ");
+                            for (int i = 0; i < items.Length; i++)
+                            {
+                                items[i] = new Vector(2);
+                                items[i].Random();
+                                items[i].OutputArr();
+                            }
+                            status = 1;
+                            Console.WriteLine("Статус операции: " + status);
                             break;
                         case 2:
-                            try
-                            {
-                                items1.OutputArr();
-                                items1.status = 1;                               
-                                items1.OutputStatus();
-                            }
-                            catch
-                            {
-                                items1.status = 0;
-                                items1.OutputStatus();
-                            }
-                            break;
-                        case 3:
-                            try
-                            {
                                 Console.Write("\nВведите число для сложения: ");
                                 int x = int.Parse(Console.ReadLine());
-                                items1.Addition(x);
-                                items1.status = 1;
-                                items1.OutputStatus();
-                            }
-                            catch
-                            {
-                                items1.status = 0;
-                                items1.OutputStatus();
-                            }
+                                for (int i = 0; i < items.Length; i++)
+                                {
+                                    items[i].Addition(x);
+                                    items[i].OutputArr();
+                                }
+                                status = 1;
+                                Console.WriteLine("Статус операции: " + status);
                             break;
-                        case 4:
-                            try
-                            {
+                        case 3:
                                 Console.Write("\nВведите число для умножения: ");
                                 int q = int.Parse(Console.ReadLine());
-                                items1.Multiplication(q);
-                                items1.status = 1;
-                                items1.OutputStatus();
-                            }
-                            catch
+                                for (int i = 0; i < items.Length; i++)
+                                {
+                                    items[i].Multiplication(q);
+                                    items[i].OutputArr();
+                                }
+                                status = 1;
+                                Console.WriteLine("Статус операции: " + status);
+                            break;
+                        case 4:
+                            bool hasNull = true;
+                            for (int i = 0; i < items.Length; i++)
                             {
-                                items1.status = 0;
-                                items1.OutputStatus();
+                                if (items[i].hasNull())
+                                {
+                                    if (hasNull)
+                                    {
+                                        Console.WriteLine("Список векторов, содержащих нули:");
+                                        hasNull = !hasNull;
+                                    }
+                                    items[i].OutputArr();
+                                    status = 1;
+                                    Console.WriteLine("Статус операции: " + status);
+                                }
                             }
+                            if (hasNull)
+                            {
+                                Console.WriteLine("Нет векторов, содержащих 0.");
+                                status = 0;
+                                Console.WriteLine("Статус операции: " + status);
+                            }
+                            break;
+                        case 5:
+                            int minModIndex = 0;
+                            for (int i = 1; i < items.Length; i++)
+                            {
+                                if (items[i].Module() < items[minModIndex].Module())
+                                {
+                                    minModIndex = i;
+                                }
+                            }
+                            Console.WriteLine("Вектор с наименьшим модулем:");
+                            items[minModIndex].OutputArr();
+                            Console.Write("Модуль: ");
+                            Console.WriteLine(items[minModIndex].Module());
                             break;
                     }
                 }
